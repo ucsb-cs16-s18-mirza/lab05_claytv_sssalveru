8198aa911fb1db3a818ae28dff9aabc8b2481d1e
claytv <38049528+claytv@users.noreply.github.com>
2018-05-08 19:39:05 -0700

    Add files via upload

---
+++ utility.cpp
@@ -0,0 +1,24 @@
+// utility.cpp
+
+// IN THIS FILE, define any of your OWN functions you may need to 
+// solve the problems.    
+
+
+// You will need to include the function prototype in "utility.h" and
+// then be sure to  #include "utility.h" in the file where you use
+// these functions
+
+#include "utility.h"
+#include <cmath> 
+
+bool numsApproxEqual(double num1, double num2, double tolerance){
+	if( fabs(num1-num2) > tolerance){
+		return true;
+	}
+	else{
+		return false;
+
+	}
+
+
+}
---
+++ utility.h
@@ -0,0 +1,15 @@
+// utility.h
+
+// IN THIS FILE, declare (function prototype only) any of your 
+// OWN functions you may need to solve the problems.    
+
+// You will need to include the function prototype in "utility.h" and
+// then be sure to  #include "utility.h" in the file where you use
+// these functions
+#ifndef UTILITY_H_
+#define UTILITY_H_
+
+bool numsApproxEqual(double num1, double num2, double tolerance);
+
+
+#endif 
